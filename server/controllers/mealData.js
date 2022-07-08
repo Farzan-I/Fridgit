323549a72fd70a214fed58cbe4f89c4e4e949236
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-07-08 12:44:10 +0100

    removed redundant route

--- server/controllers/mealData.js
+++ server/controllers/mealData.js
@@ -1,11 +1,5 @@
 import fetchMealData from '../externalApi/mealData.js'
 
-export const searchMealDB = (req, res) => {
-  console.log(req.body)
-  fetchMealData()
-  .then(response => res.send(response))
-}
-
 export const getMealData = (req, res) => {
   console.log(req.body.search)
   fetchMealData(req.body.search)
--- server/routes/mealData.js
+++ server/routes/mealData.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
-import { getMealData, searchMealDB } from '../controllers/mealData.js'
+import { getMealData } from '../controllers/mealData.js'
 
 const router = express.Router();
 
 router.post('/', getMealData);
-router.post('/search', searchMealDB);
 
 export default router;
