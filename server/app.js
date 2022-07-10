91630078218a302abe582b90763edeeb1cfcdad8
S-Spiegl <steven.spiegl@gmail.com>
2022-07-10 12:51:22 +0100

    figured out mongoose connection problem, I think, have not fixed it

--- server/app.js
+++ server/app.js
@@ -28,25 +28,28 @@ app.use('/instructions', instructionDataRoutes)
 
 // this is connection is failing. to be investigated
 const CONNECTION_URL = "mongodb+srv://BrainsOnBrioche:spinnerdolphin@cluster0.ohaz6.mongodb.net/?retryWrites=true&w=majority";
-//have got it down to a port-in-use error... but port isn't in use. It throws that error for every attempted connection
+//I don't think this is about the IP address permissions... changed it to allow access from any ip address... 
+//seemed to maybe change things a little, not sure, but in any case, hasn't fixed it...
+
+//have got it down to a port-in-use error... but port ISN'T in use. It throws that error for every attempted connection
 //every time I kill the port, if I then run npm start for the client, it puts that port back into use with a new PID... 
-//so it just becomes an endless cycle
-//changed it to allow access from any ip address... seemed to maybe change things a little, not sure
+//so it just becomes an endless cycle. I changed all the 4000s to 8082s... still didn't work. Whether the port is in use or not 
+//doesn't seem to matter when both of the app.listens are uncommented...
+
 const PORT = process.env.PORT || 4000;
 
 //can also comment out the line below and then connect to mongoose at the bottom... but does this prevent the apis from working?
+//I think maybe it is the fact that it's listening twice on the same port that was crashing it? Did we add the above line in after
+//we added in the mongoose.connect? I think we did... this might have been when it broke... if it's listening on port with app.listen,
+//then when we run mongoose.connect, the port is already in use?? This can be illustrated by commenting out the app.listen under mongoose.connect - 
+//it still works...
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+// app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 
 // this is connection is failing. to be investigated
-//I don't think this is about the IP address... SHS
-//is it worth jumping back to an earlier commit when it was working? I feel like this broke around the same time we started having
-//to add .js to everything/introduce babel etc... 
-//changed all the 4000s to 8082s... still didn't work. Whether the port is in use or not doesn't seem to matter when the below
-//is uncommented...
 
-// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-//   .catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
 
 // mongoose.set('useFindAndModify', false);
