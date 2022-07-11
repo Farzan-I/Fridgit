be3ac9625ae62af805b98215b4d966e087bfd9cf
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-07-11 10:46:04 +0100

    name renders to page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 export default function Navbar() {
   
   // Can't access the state. It may not be saving properly.
+  // Add event listener?
   const userData  = useSelector((state) => state.user)
   console.log("Navbar component: " + userData)
 
@@ -11,6 +12,8 @@ export default function Navbar() {
     <nav>
       <img className="nav--logo" src='./brains-on-brioche.png' alt="logo"></img>
       <h3 className="nav--title">Brains on Brioche</h3>
+      {userData.userName && <h2 className="nav--username">Hi, {userData.userName}!</h2>}
     </nav>
   )
 }
+
