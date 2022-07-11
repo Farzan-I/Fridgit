5528667ea9f923ef866c29466b00483465f34167
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-07-11 10:47:57 +0100

    removed redundant console logs

--- client/src/actions/addUser.js
+++ client/src/actions/addUser.js
@@ -3,7 +3,6 @@ import * as api from '../api/index.js'
 export const addUser = (userData) => async (dispatch) => {
   try {
     const { data } = await api.addUser({userData: userData});
-    console.log("Actions log: " + data.userName)
     dispatch({ type: 'SEND_USER_DATA', payload: data });
   } catch (error) {
     console.log(error.message);
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,11 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function Navbar() {
-  
-  // Can't access the state. It may not be saving properly.
-  // Add event listener?
   const userData  = useSelector((state) => state.user)
-  console.log("Navbar component: " + userData)
 
   return (
     <nav>
