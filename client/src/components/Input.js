a8e49ac5d613768ed8186ef64cd7950bd2c4cdd4
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-07-13 15:19:58 +0100

    can toggle between search results

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -7,12 +7,15 @@ import { useDispatch, useSelector } from 'react-redux';
 const Input = () => {
 
   const userFridge = useSelector((state) => state.user.fridge)
-
+  
   const [searchData, setSearchData] = useState({
     ingredients: ''
   })
-
-  console.log(userFridge)
+  const [isChecked, setIsChecked] = useState(false);
+  
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  };
 
   // const mealsUrl = 'http://localhost:4000/meals/search';
 
@@ -26,14 +29,21 @@ const Input = () => {
   const handleSubmit = async () => {
     dispatch(getMealData(
       userFridge.join(","),
-      2
+      isChecked ? 1 : 2
       ));
   };
 
   return (
     <div>
       <h1 className="input--header">What's in your fridge?</h1>
-
+      <div>
+        <h3>How do you want to use your ingredients?</h3>
+        {isChecked ? "Use as many as I can" : "See recipes I have all the ingredients for"}
+        <label class="switch">
+        <input type="checkbox" checked={isChecked} onChange={handleOnChange}/>
+        <span class="slider round"></span>
+      </label>
+      </div>
       <div>
         <button className="form--button" onClick={handleSubmit}>What's for Dinner?</button>
       </div>
